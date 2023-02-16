1f6f1b1a8c552cab2ac408063368843343dc9711
Mavri <74113150+LeoMavri@users.noreply.github.com>
2023-02-16 09:57:24 +0200

    misc: disabled all unused TeleOPs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/HomosapiensTeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/HomosapiensTeleOpTest.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -20,6 +21,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  * encoder localizer heading may be significantly off if the track width has not been tuned).
  */
 @Config
+@Disabled
 @TeleOp(name = "HomosapiensTeleOPTest", group = "testing")
 public class HomosapiensTeleOpTest extends LinearOpMode {
     public double SPEED_MULTIPLIER = 0.75;
